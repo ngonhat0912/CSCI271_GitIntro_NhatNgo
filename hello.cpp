46aad73e2e006b340b47dc5d7fda9d32e321db1c
ngonhat0912 <ngonhat0912@gmail.com>
2026-06-02 00:21:40 -0400

    Add hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2870
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww19820\viewh17160\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 // CSCI 27X - GitHub Upload Practice  \
+// Name: Nhat Ngo\
+// Section: CSCI 271\
+  \
+#include <iostream>  \
+using namespace std;  \
+  \
+int main() \{  \
+    cout << "Hello from GitHub! My first commit works." << endl;  \
+    return 0;  \
+\}}
